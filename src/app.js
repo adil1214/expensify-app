24e3b3d1b00d8e25d74bba03eff590f700797ee3
adil1214 <adil1350@gmail.com>
2018-07-08 19:45:46 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+let appRoot = document.getElementById('app');
+ReactDOM.render(<p>this is a template</p> , appRoot);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+// let absPath = path.join(__dirname, '/public');
+// console.log(absPath)
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 1234,
+        open: true,
+        openPage: ''    // <= webpack dev server bug workaround
+    }
+};
