713e677aabf580580d22f014f30ed1287f30c656
adil1214 <adil1350@gmail.com>
2018-07-15 22:08:22 +0100

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,15 +1,61 @@
-const person = {
-  // name: 'Andrew',
-  age: 33,
-  location: {
-    city: 'hungary',
-    temp: 26,
-  }
-};
+//
+// ARRAY DESTRUCTURING
+//
 
-const { name: firstName = 'Anonymous', age } = person;
-const { city, temp: temperature } = person.location;
 
-console.log(`${firstName} is ${age}. `);
+// street, city, state, zip
+const address = ['1345 s senior street', 'Pheladilphia', 'Penselvenya', '19874'];
 
-console.log(`it's ${temperature} in ${city} `);
+// you can set up defaults like in object destructuring.
+// const [street, city, state, zip] = address;
+const [, , state = 'New york'] = address;
+
+console.log(`You are in ${state} .`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =========================================================
+// OBJECT DESTRUCTURING
+//
+
+// const person = {
+//   // name: 'Andrew',
+//   age: 33,
+//   location: {
+//     city: 'hungary',
+//     temp: 26,
+//   }
+// };
+
+// const { name: firstName = 'Anonymous', age } = person;
+// const { city, temp: temperature } = person.location;
+
+// console.log(`${firstName} is ${age}. `);
+
+// console.log(`it's ${temperature} in ${city} `);
