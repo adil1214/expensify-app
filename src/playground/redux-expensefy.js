d3c540bf3241adc40dcc3191aea76f364c0eb2d3
adil1214 <adil1350@gmail.com>
2018-08-04 11:17:04 +0100

    working with multiple reducers

--- src/playground/redux-expensefy.js
+++ src/playground/redux-expensefy.js
@@ -1,4 +1,46 @@
 import { createStore, combineReducers } from 'redux';
+import uuidv1 from 'uuid/v1';
+
+// ADD_EXPENSE
+const addExpense = (
+    { 
+      description = '', 
+      note = '', 
+      amount = 0,
+      createdAt = 0
+    } = {}
+  ) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuidv1(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+// REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+const expensesReducerDefaultState = [];
+
+const filterReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
 
 const demoState = {
   expenses: [{
@@ -16,15 +58,40 @@ const demoState = {
   }
 };
 
-const expensesReducer = (state = [], action) => {
+// Expenses reducer
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter( ({ id }) => action.id !== id );
+    default:
+      return state;
+  }
+};
+
+// Filters reducer
+const filtersReducer = (state = filterReducerDefaultState, action) => {
   switch (action.type) {
     default:
       return state;
   }
 };
 
-const store = createStore(expensesReducer);
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 })
+
+const expenseOne = store.dispatch(addExpense({ description: 'coffee', amount: 200 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'rent', amount: 60000 }));
+
+// console.log(expenseOne.expense.id);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
