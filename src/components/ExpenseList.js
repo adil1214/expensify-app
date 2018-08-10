6d2f3abe51d79868ccb3eeb34404d7407dc0fcf4
adil1214 <adil1350@gmail.com>
2018-08-10 16:52:38 +0100

    worked by passing the dispatch function down as a prop

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,14 +7,42 @@ import { addExpense, editExpense, removeExpense } from './../actions/expenses';
 
 // convert this into a class based component and pass down an event handler 
 // function that accepts a parameter(the bill to delete ) ???
-const ExpenseList = (props) => (
-  <div>
-    <h1>Expense List</h1>
-    {props.expenses.map((a, b) => {
-      return <ExpenseListItem {...a} key={b} dispatchFunc={props.dispatch} /> 
-    })}
-  </div>
-);
+
+// const ExpenseList = (props) => (
+//   <div>
+//     <h1>Expense List</h1>
+//     {props.expenses.map((a, b) => {
+//       return <ExpenseListItem {...a} key={b} dispatchFunc={props.dispatch} /> 
+//     })}
+//   </div>
+// );
+
+class ExpenseList extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.dispatchFunc = this.dispatchFunc.bind(this);
+  }
+
+  // dispatchFunc(id) {
+  //   console.log(`expense with id ${id} should be removed `); 
+  //   // console.log(`type of id: ${typeof id} `);
+  //   this.props.dispatch(removeExpense(id));
+  // }
+
+  render() {
+    return (
+        <div>
+          <h1>Expense List</h1>
+          {this.props.expenses.map((a, b) => {
+            return <ExpenseListItem key={b} dispatchFunc={this.props.dispatch} {...a} /> 
+          })}
+      </div>
+    );
+  }
+}
+
+
 
 const mapStateToProps = (state) => {
   return {
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import { addExpense, editExpense, removeExpense } from './../actions/expenses';
 
 export default ({id, description, amount, createdAt, dispatchFunc}) => {
@@ -8,8 +8,8 @@ export default ({id, description, amount, createdAt, dispatchFunc}) => {
     <b>amount</b>:{amount}<br/>
     <b>created at</b>: {createdAt}<br/></p>
     <button onClick={(e) => {
-      e.preventDefault();
-      dispatchFunc(removeExpense(id));
+      dispatchFunc(removeExpense({id}));
+      console.log(`expense with id ${id} should be removed `); 
     }}>Remove</button>
     <hr/>
   </div>;
