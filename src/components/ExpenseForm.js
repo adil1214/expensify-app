7260f049a3292ccfe24bbe2eb6a3dcfa6675de86
adil1214 <adil1350@gmail.com>
2018-08-11 19:29:21 +0100

    added date picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends Component {
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    focused: false
   };
 
   onDescriptionChange = (e) => {
@@ -26,35 +32,51 @@ export default class ExpenseForm extends Component {
 
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if ( amount.match(/^\d*(\.\d{0,2})?$/) ) {
+    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }))
     }
   }
+
+  onDateChange = (createdAt) => this.setState({ createdAt });
+
+  onFocusChange = ({ focused }) => this.setState({ focused });
 
   render() {
     return (
       <div>
         <form action="">
-          <input 
+          <input
             type="text"
             placeholder="description"
             autoFocus="true"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
-          <input 
+
+          <input
             type="text"
             placeholder="amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
+
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.focused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={(day) => false }
+          />
+
           <textarea
             placeholder="add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
           </textarea>
-            <button>Add Expense</button>
+
+          <button>Add Expense</button>
         </form>
       </div>
     );
