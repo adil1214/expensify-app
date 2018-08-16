1f8a9593914088db1a7a985d683172d78e356576
adil1214 <adil1350@gmail.com>
2018-08-16 20:13:59 +0100

    replaced timestamps with YYYY-MM-DD date format.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = ({id, description, amount, createdAt, note}) => {
   return <div>
     <p><b>description:</b><Link to={`/edit/${id}`}>{description}</Link><br/>
     <b>amount:</b>{amount}<br/>
-    <b>created at</b>: {createdAt}<br/>
+    {/* <b>created at</b>: {createdAt}<br/> */}
+    <b>normal date</b>: {moment(createdAt).format('YYYY-MM-DD')}<br/>
     <b>note:</b> {note}<br/></p>
     <hr/>
   </div>;
