0f9f85750e577730146e14d8c1ae2388676d1f70
adil1214 <adil1350@gmail.com>
2018-08-23 15:50:12 +0100

    source map now depends on the env (dev or prod)

--- src/app.js
+++ src/app.js
@@ -12,21 +12,6 @@ import './styles/styles.scss';
 
 const store1 = configureStore();
 
-store1.subscribe(() => {
-  const currentState = store1.getState();
-  const visibleExpenses = getVisibleExpenses(currentState.expenses, currentState.filters);
-  console.log(visibleExpenses);
-});
-
-//==dummy data==
-store1.dispatch(addExpense({description: 'water bill', amount: 500, createdAt: moment('2018-08-10').valueOf() }));
-store1.dispatch(addExpense({description: 'gas bill', amount: 700, createdAt: 90000000}));
-store1.dispatch(addExpense({description: 'electricity bill', amount: 900}));
-store1.dispatch(addExpense({description: '4th week\'s food', amount: 350, createdAt: 1000}));
-
-store1.dispatch(setTextFilter('bill'));
-//==============
-
 const jsx = (
   <Provider store={store1}>
     <AppRouter />
