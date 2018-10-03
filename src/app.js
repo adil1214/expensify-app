7bb6dfdb3222478dda9bb7c30739c98a1b15be44
adil1214 <adil1350@gmail.com>
2018-10-03 23:17:45 +0100

    added formating for amount

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import moment from 'moment';
-import { addExpense, editExpense, removeExpense } from './actions/expenses';
-import { setTextFilter, setStartDate, setEndDate, sortByAmount, sortByDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import moment from 'moment';
+// import { addExpense, editExpense, removeExpense } from './actions/expenses';
+// import { setTextFilter, setStartDate, setEndDate, sortByAmount, sortByDate } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -14,9 +14,9 @@ import 'react-dates/lib/css/_datepicker.css';
 const store1 = configureStore();
 
 const jsx = (
-  <Provider store={store1}>
-    <AppRouter />
-  </Provider>
+	<Provider store={store1}>
+		<AppRouter />
+	</Provider>
 );
 
 let appRoot = document.getElementById('app');
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, createdAt, note}) => {
   return <div>
     <p><b>description:</b><Link to={`/edit/${id}`}>{description}</Link><br/>
-    <b>amount:</b>{amount}<br/>
+    <b>amount: </b>{numeral(amount/100).format('$0,0.00')}<br/>
     {/* <b>created at</b>: {createdAt}<br/> */}
     <b>normal date</b>: {moment(createdAt).format('YYYY-MM-DD')}<br/>
     <b>note:</b> {note}<br/></p>
