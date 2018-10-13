d0edd9ea63bedea4ee7095daba4c6f5b4a1d3ae6
adil1214 <adil1350@gmail.com>
2018-10-13 13:27:00 +0100

    replaced addExpense with startAddExpense thunk

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import { connect } from 'react-redux';
-import { addExpense, editExpense, removeExpense } from './../actions/expenses';
+import { startAddExpense, editExpense, removeExpense } from './../actions/expenses';
 
 const AddExpensePage = (props) => (
     <div>
         This is from my <strong>add expense</strong> component.
         <ExpenseForm
             onSubmit={(expense) => {
-                props.dispatch(addExpense(expense));
+                props.dispatch(startAddExpense(expense));
                 props.history.push('/');
             }}
         />
