4cbb341ca5e28b3b08a9fc81796fee8dcd3def07
adil1214 <adil1350@gmail.com>
2018-10-13 13:26:27 +0100

    added redux-thunk and redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import expensesReducer from './../reducers/expenses';
 import filtersReducer from './../reducers/filters';
 
 export default () => {
-  const store = createStore(
-    combineReducers({
-      expenses: expensesReducer,
-      filters: filtersReducer
-    }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-  
-  return store;
+	const store = createStore(
+		combineReducers({
+			expenses: expensesReducer,
+			filters: filtersReducer
+		}),
+		composeWithDevTools(applyMiddleware(thunk, logger))
+	);
+
+	return store;
 };
