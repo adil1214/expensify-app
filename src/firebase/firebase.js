5ac401fb93d50fbb6619c5121322263f31f4dc49
adil1214 <adil1350@gmail.com>
2018-10-14 11:53:19 +0100

    separate databases depending on the env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: 'AIzaSyCaDhYdbhg60WygApmj6khysq_D1rViCaE',
-	authDomain: 'expensefy-2cfd1.firebaseapp.com',
-	databaseURL: 'https://expensefy-2cfd1.firebaseio.com',
-	projectId: 'expensefy-2cfd1',
-	storageBucket: 'expensefy-2cfd1.appspot.com',
-	messagingSenderId: '8122225891'
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
