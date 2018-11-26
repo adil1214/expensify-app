120c669402861a79012b0c08c6100e3135d292c3
adil1214 <adil1350@gmail.com>
2018-11-26 23:14:30 +0100

    styling list filters

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,67 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './../actions/filters'
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './../actions/filters';
 import { DateRangePicker } from 'react-dates';
 import moment from 'moment';
 
 class ExpenseListFilters extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      focusedInput: null
-    }
-  }
+	constructor(props) {
+		super(props);
 
-  onDatesChange = ({ startDate, endDate }) => {
-    this.props.dispatch(setStartDate(startDate));
-    this.props.dispatch(setEndDate(endDate));
-  };
+		this.state = {
+			focusedInput: null
+		};
+	}
 
-  onFocusChange = (focusedInput) => {
-    this.setState(() => ({ focusedInput }));
-  };
+	onDatesChange = ({ startDate, endDate }) => {
+		this.props.dispatch(setStartDate(startDate));
+		this.props.dispatch(setEndDate(endDate));
+	};
 
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={(e) => {
-            this.props.dispatch(setTextFilter(e.target.value));
-          }}
-        />
+	onFocusChange = (focusedInput) => {
+		this.setState(() => ({ focusedInput }));
+	};
 
-        <select
-          value={this.props.filters.sortBy}
-          onChange={(e) => {
-            if (e.target.value === 'date') {
-              this.props.dispatch(sortByDate())
-            } else if (e.target.value === 'amount') {
-              this.props.dispatch(sortByAmount())
-            }
-          }
-          }>
-          <option value="date">Date</option>
-          <option value="amount">Amount</option>
-        </select>
-
-        <DateRangePicker
-          startDate={this.props.filters.startDate} // momentPropTypes.momentObj or null,
-          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-          endDate={this.props.filters.endDate} // momentPropTypes.momentObj or null,
-          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-          onDatesChange={this.onDatesChange} // PropTypes.func.isRequired,
-          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-          onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
-          isOutsideRange={(day) => false }
-          numberOfMonths={1}
-          showClearDates={true}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="content-container">
+				<div className="input-group">
+					<div className="input-group__item">
+						<input
+							type="text"
+							value={this.props.filters.text}
+							onChange={(e) => {
+								this.props.dispatch(setTextFilter(e.target.value));
+							}}
+						/>
+					</div>
+					<div className="input-group__item">
+						<select
+							value={this.props.filters.sortBy}
+							onChange={(e) => {
+								if (e.target.value === 'date') {
+									this.props.dispatch(sortByDate());
+								} else if (e.target.value === 'amount') {
+									this.props.dispatch(sortByAmount());
+								}
+							}}
+						>
+							<option value="date">Date</option>
+							<option value="amount">Amount</option>
+						</select>
+					</div>
+					<div className="input-group__item">
+						<DateRangePicker
+							startDate={this.props.filters.startDate} // momentPropTypes.momentObj or null,
+							startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+							endDate={this.props.filters.endDate} // momentPropTypes.momentObj or null,
+							endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+							onDatesChange={this.onDatesChange} // PropTypes.func.isRequired,
+							focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+							onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
+							isOutsideRange={(day) => false}
+							numberOfMonths={1}
+							showClearDates={true}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 // const ExpenseListFilters = (props) => (
@@ -69,9 +75,9 @@ class ExpenseListFilters extends Component {
 // );
 
 const mapStateToProps = (state) => {
-  return {
-    filters: state.filters
-  }
+	return {
+		filters: state.filters
+	};
 };
 
 export default connect(mapStateToProps)(ExpenseListFilters);
