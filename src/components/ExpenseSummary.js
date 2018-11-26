f9ca93980b57241c6b852a1346e7529926b6b4c1
adil1214 <adil1350@gmail.com>
2018-11-26 22:10:37 +0100

    buttons styled

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 import expensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/expenses';
 
 class ExpenseSummary extends Component {
 	render() {
-    const expString = this.props.expensesCount === 1 ? 'expense' : 'expenses';
-    const formattedTotal = numeral(this.props.expensesTotal/100).format('$0,0.00');
+		const expString = this.props.expensesCount === 1 ? 'expense' : 'expenses';
+		const formattedTotal = numeral(this.props.expensesTotal / 100).format('$0,0.00');
 		return (
-			<div>
-				<h2>Viewing {this.props.expensesCount} {expString}, totaling {formattedTotal}</h2>
+			<div className="page-header">
+				<div className="content-container">
+					<h1 className="page-header__title">
+						Viewing <span>{this.props.expensesCount}</span> {expString}, totaling <span>{formattedTotal}</span>
+					</h1>
+					<div className="page-header__actions">
+						<Link className="button" to="/create">Add Expense</Link>
+					</div>
+				</div>
 			</div>
 		);
 	}
