48f5fa65852cf963e92334b3b2d495069f0c69b5
adil1214 <adil1350@gmail.com>
2019-01-11 17:42:04 +0100

    compression webpack plugin

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const publicPath = path.join(__dirname, '..', 'public')
 
 app.use(express.static(publicPath));
 
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
