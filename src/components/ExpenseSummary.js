8df6aaa0704c9e7e7c2ecd899e05bfc078269b5f
adil1214 <adil1350@gmail.com>
2019-01-11 19:43:49 +0100

    now showing a fraction (viewing/total) in the summary

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -7,16 +7,21 @@ import getVisibleExpenses from '../selectors/expenses';
 
 class ExpenseSummary extends Component {
 	render() {
-		const expString = this.props.expensesCount === 1 ? 'expense' : 'expenses';
-		const formattedTotal = numeral(this.props.expensesTotal / 100).format('$0,0.00');
+		const { expensesCount, expensesTotal, allExpensesCardinal } = this.props;
+		const expString = expensesCount === 1 ? 'expense' : 'expenses';
+		const formattedTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
 		return (
 			<div className="page-header">
 				<div className="content-container">
 					<h1 className="page-header__title">
-						Viewing <span>{this.props.expensesCount}</span> {expString}, totaling <span>{formattedTotal}</span>
+						Viewing <span>{`${expensesCount} / ${allExpensesCardinal}`} </span> {expString}, totaling{' '}
+						<span>{formattedTotal}</span>
 					</h1>
 					<div className="page-header__actions">
-						<Link className="button" to="/create">Add Expense</Link>
+						<Link className="button" to="/create">
+							Add Expense
+						</Link>
 					</div>
 				</div>
 			</div>
@@ -28,7 +33,8 @@ const mapStateToProps = (state) => {
 	let visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 	return {
 		expensesCount: visibleExpenses.length,
-		expensesTotal: expensesTotal(visibleExpenses)
+		expensesTotal: expensesTotal(visibleExpenses),
+		allExpensesCardinal: state.expenses.length
 	};
 };
 
